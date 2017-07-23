eb924a07c667a6bdaa47bef4c7e7ca0716afe64f
Jiamin Wang <jw2gd@mail.missouri.edu>
2017-07-23 01:10:43 -0400

    Add files via upload

---
+++ MiniMat GNU/MiniMat.cpp
@@ -0,0 +1,13 @@
+/*
+*File Name: MiniMat.cpp
+*File Creation Date: April 17th 2017
+*File Final Revised Date: May 7th 2017
+*Author: Jiamin Wang
+*Description: Source of MiniMat
+*/
+
+#include "MiniMat.hpp"
+
+#if __HEAPSCOPE
+	_Mat_Heap MiniMat_HeapScope;
+#endif
